a5d30ac2f362d62212ac5792fd01551e2bfd8bc9
Abhijeet Anand <abhijeet.anand@zeptonow.com>
2025-10-24 02:27:19 +0530

    Update branding: Replace Brittany Chiang with Abhijeet Anand

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Abhijeet Anand',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Abhijeet Anand is a Data Scientist specializing in machine learning, deep learning, and building scalable ML systems for real-world impact.',
+    siteUrl: 'https://abhijeetanand29.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@abhijeetanand',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Abhijeet Anand',
+        short_name: 'Abhijeet Anand',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
